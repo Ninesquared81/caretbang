4b5ff02577ef60f24d4522128e0f0f2dee337f98
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2022-09-30 20:20:22 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include "lexer.h"
+#include "sizelims.h"
 
 
 int main(int argc, char **argv) {
   (void)argc; (void)argv;
-  printf("Hello, World!\n");
+  struct token tokens[TOKENS_MAX];
+  ssize_t n = lex("", tokens);
+  if (n == -1) {
+    fprintf(stderr, "Maximum token limit reached.\n");
+    exit(EXIT_FAILURE);
+  }
   return EXIT_SUCCESS;
 }
