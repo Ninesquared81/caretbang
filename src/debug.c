9c4b50c9d48829f3c0dc2d323944d8192c602594
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2022-11-18 04:24:41 +0000

    Fix bug in compiler_error()

--- src/debug.c
+++ src/debug.c
@@ -6,13 +6,17 @@
 
 int compiler_error_function(const char *restrict file, const char *restrict func,
 			    const int line, const char *restrict msg, ...) {
-  fprintf(stderr, "[COMPILER ERROR] ");
-
   va_list args;
   va_start(args, msg);
-  fprintf(stderr, msg, args);
+  int ret = v_compiler_error_function(file, func, line, msg, args);
   va_end(args);
-  
+  return ret;
+}
+
+int v_compiler_error_function(const char *restrict file, const char *restrict func,
+			      const int line, const char *restrict msg, va_list args) {
+  fprintf(stderr, "[COMPILER ERROR] ");
+  vfprintf(stderr, msg, args);
   fprintf(stderr,
 	  "\n..in function %s()\n..in file '%s'\n..on line %d\n\n"
 	  "NOTE: This is a compiler bug, not an issue with your program.\n",
--- src/debug.h
+++ src/debug.h
@@ -6,6 +6,7 @@
 #define compiler_limit(MSG, ...) compiler_limit_function(__FILE__, __func__, __LINE__, MSG, __VA_ARGS__)
 
 
+/* full function versions */
 int compiler_error_function(const char *restrict file, const char *restrict func,
 			    const int line, const char *restrict msg, ...);
 
@@ -14,5 +15,16 @@ int memory_error_function(const char *restrict file, const char *restrict func,
 
 int compiler_limit_function(const char *restrict file, const char *restrict func,
 			    const int line, const char *restrict msg, ...);
+
+
+/* va_list versions */
+int v_compiler_error_function(const char *restrict file, const char *restrict func,
+			      const int line, const char *restrict msg, va_list args);
+
+int v_memory_error_function(const char *restrict file, const char *restrict func,
+			    const int line, const char *restrict msg, va_list args);
+
+int v_compiler_limit_function(const char *restrict file, const char *restrict func,
+			      const int line, const char *restrict msg, va_list args);
 
 #endif
