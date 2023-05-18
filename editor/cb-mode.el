d58a13661124c32bcd2bf6d20c77785f81a4809f
Ninesquared81 <lmspatriotdofs@gmail.com>
2023-05-18 02:54:00 +0100

    Implement Emacs major mode for ^!

--- editor/cb-mode.el
+++ editor/cb-mode.el
@@ -1,4 +1,31 @@
 ;;; cb-mode.el --- Major mode for ^! -*- lexical-binding: t -*-
 
-(provide 'cb-mode)
+(defconst cb-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?\[ "(]" st)
+    (modify-syntax-entry ?\] ")[" st)
+    (modify-syntax-entry ?\( "<)nb" st)
+    (modify-syntax-entry ?\) ">(nb" st)
+    st)
+  "Syntax table for cb-mode")
+
+(defconst cb-instructions
+  '("^" "!" "+" "-" "," "." "[" "]" ":" "*" "%" "@")
+  "^! reserved instruction characters")
+
+(defconst cb-highlighting
+  `((,(regexp-opt cb-instructions t) . font-lock-keyword-face))
+  "Instruction highlighting regex for ^!")
+
+;;;###autoload
+(define-derived-mode cb-mode prog-mode "^!"
+  "Major mode for ^!"
+  :syntax-table cb-mode-syntax-table
+  (setq font-lock-defaults '(cb-highlighting)))
+
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.cb\\'" . cb-mode))
+
+;(provide 'cb-mode)
 ;;; cb-mode.el ends here
